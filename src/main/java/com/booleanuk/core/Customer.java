f5feee628c400b5ef673f994c2d5a1a93a2b3dfd
David <davve.bylund@gmail.com>
2026-08-26 15:54:27 +0200

    greeen: customer done, everyting in core done?

--- src/main/java/com/booleanuk/core/Customer.java
+++ src/main/java/com/booleanuk/core/Customer.java
@@ -1,6 +1,30 @@
 package com.booleanuk.core;
 
 public class Customer {
+	
+	private Bank bank;
 
-    
+	public Customer(Bank bank){
+		setBank(bank);
+	}
+
+	private void setBank(Bank bank){
+		this.bank = bank;
+	}
+
+	private Bank getBank(){
+		return this.bank;
+	}
+
+	public Account createCurrentAccount(String name) throws Exception{
+		return getBank().requestAccount(name, "CUR");
+	}
+
+	public Account createSavingsAccount(String name) throws Exception{
+		return getBank().requestAccount(name, "SAV");
+	}
+
+	public Account getAccount(String name) throws Exception{
+		return getBank().getAccount(name);
+	}
 }
--- src/test/java/com/booleanuk/core/CustomerTest.java
+++ src/test/java/com/booleanuk/core/CustomerTest.java
@@ -13,24 +13,31 @@ public class CustomerTest {
 	private Customer c;
 	private Bank b;
 
-	public CustomerTest(){
+	public CustomerTest() throws Exception{
 		this.b = new Bank();
 		this.b.requestAccount("nvim funds", "SAV");
 		this.c = new Customer(this.b);
 	}
 
 	@Test
-	public void shouldCreateCurrentAccount(){
+	public void shouldCreateCurrentAccount() throws Exception{
 		Account resAcc = c.createCurrentAccount("testAccount");
 		Assertions.assertEquals("testAccount", resAcc.getName());
-		Assertions.assertEquals(CurrentAccount.class, resAcc.class);
+		Assertions.assertEquals(CurrentAccount.class, resAcc.getClass());
 	}
 
 	@Test
-	public void shouldCreateSavingsAccount(){
+	public void shouldCreateSavingsAccount() throws Exception{
 		Account resAcc = c.createSavingsAccount("testAccount");
 		Assertions.assertEquals("testAccount", resAcc.getName());
-		Assertions.assertEquals(SavingsAccount.class, resAcc.class);
+		Assertions.assertEquals(SavingsAccount.class, resAcc.getClass());
+	}
+
+	@Test
+	public void shouldGetAccount() throws Exception{
+		Account resAcc = c.getAccount("nvim funds");
+		Assertions.assertEquals("nvim funds", resAcc.getName());
+		Assertions.assertEquals(SavingsAccount.class, resAcc.getClass());
 	}
 
 }
